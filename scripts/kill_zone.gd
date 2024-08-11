e35584340a620a73c92dc40c6e29442b5b90fce8
Mathieu Agopian <mathieu@agopian.info>
2024-08-11 21:06:25 +0200

    Add coyote time jumps

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -5,7 +5,6 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	print("You died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+@export var SPEED = 100.0
+@export var JUMP_VELOCITY = -300.0
+@export var COYOTE_TIME = 0.1
+@onready var coyote_timer = $CoyoteTimer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,19 +16,28 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	handle_jump()
+	var direction : float = get_direction()	
+	handle_animations(direction)
+	apply_movement(direction)
 
-	# Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("move_left", "move_right")
+func handle_jump():
+	if Input.is_action_just_pressed("jump") and can_jump():
+		if coyote_timer.time_left:
+			print("coyote time!!")
+		velocity.y = JUMP_VELOCITY
 	
+func get_direction() -> float:
+	# Get the input direction: -1, 0, 1
+	return Input.get_axis("move_left", "move_right")
+
+func handle_animations(direction):
 	# Flip the sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-		
+
 	# Play animation
 	if is_on_floor():
 		if direction == 0:
@@ -35,10 +46,20 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-	# Apply movement
+
+func apply_movement(direction):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	var was_on_floor : bool = is_on_floor()
 	move_and_slide()
+	if was_on_floor and not is_on_floor() and not is_jumping():
+		coyote_timer.start(COYOTE_TIME)
+
+func is_jumping() -> bool:
+	return velocity.y < 0
+
+func can_jump() -> bool:
+	return is_on_floor() or coyote_timer.time_left > 0
