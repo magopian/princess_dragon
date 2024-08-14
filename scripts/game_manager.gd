eb5a7a44152b53984e905a646fde73f6090f3017
Mathieu Agopian <mathieu@agopian.info>
2024-08-14 16:41:06 +0200

    Install gdtoolkit addons (auto format), better canvas/view/camera, fix coyote time

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,9 +7,14 @@ var score = 0
 
 
 func _ready():
-	var mobile = OS.has_feature("android") or OS.has_feature("ios") or OS.has_feature("web_android") or OS.has_feature("web_ios")
+	var mobile = (
+		OS.has_feature("android")
+		or OS.has_feature("ios")
+		or OS.has_feature("web_android")
+		or OS.has_feature("web_ios")
+	)
 	touch_controls.visible = mobile
-	
+
 
 func add_point():
 	score += 1
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-@export var SPEED = 100.0
-@export var JUMP_VELOCITY = -300.0
-@export var COYOTE_TIME = 0.1
-@onready var coyote_timer = $CoyoteTimer
+@export var SPEED: float = 100.0
+@export var JUMP_VELOCITY: float = -300.0
+@export var COYOTE_TIME: float = 0.1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
@@ -17,19 +17,20 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	handle_jump()
-	var direction : float = get_direction()	
+	var direction: float = get_direction()
 	handle_animations(direction)
 	apply_movement(direction)
 
+
 func handle_jump():
 	if Input.is_action_just_pressed("jump") and can_jump():
-		if coyote_timer.time_left:
-			print("coyote time!!")
 		velocity.y = JUMP_VELOCITY
-	
+
+
 func get_direction() -> float:
 	# Get the input direction: -1, 0, 1
 	return Input.get_axis("move_left", "move_right")
+
 
 func handle_animations(direction):
 	# Flip the sprite
@@ -47,19 +48,26 @@ func handle_animations(direction):
 	else:
 		animated_sprite.play("jump")
 
+
 func apply_movement(direction):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	var was_on_floor : bool = is_on_floor()
+	var was_on_floor: bool = is_on_floor()
 	move_and_slide()
 	if was_on_floor and not is_on_floor() and not is_jumping():
 		coyote_timer.start(COYOTE_TIME)
 
+
 func is_jumping() -> bool:
 	return velocity.y < 0
 
+
 func can_jump() -> bool:
-	return is_on_floor() or coyote_timer.time_left > 0
+	if is_on_floor():
+		return true
+	if coyote_timer.time_left > 0:
+		return true
+	return false
