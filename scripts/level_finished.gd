b22664d7fe4eb7e7b991573fe9eb67b05e20a2d3
Mathieu Agopian <mathieu@agopian.info>
2024-09-13 15:43:45 +0200

    Display the level name in the level finished screen

--- scripts/level_finished.gd
+++ scripts/level_finished.gd
@@ -5,10 +5,12 @@ extends CanvasLayer
 @onready var score_visible: bool
 @onready var coins_collected: Label = %CoinsCollected
 @onready var time_elapsed: Label = %TimeElapsed
+@onready var level_name: Label = %LevelName
 
 
 func _ready() -> void:
 	next_level.grab_focus()
+	level_name.text = GameManager.current_level.name
 	coins_collected.text = str(GameManager.get_level_coins())
 	time_elapsed.text = str(GameManager.get_time_elapsed() / 1000)
 
