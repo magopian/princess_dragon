d22d6e2c6e9682cd3ac39c509bfe8e05d1a49769
Mathieu Agopian <mathieu@agopian.info>
2024-09-14 10:30:33 +0200

    Display a "NEW!" label when new best score

--- scripts/level_finished.gd
+++ scripts/level_finished.gd
@@ -3,21 +3,25 @@ extends CanvasLayer
 @export var level: Node2D
 @onready var next_level: Button = %NextLevel
 @onready var score_visible: bool
-@onready var coins_collected: Label = %CoinsCollected
-@onready var time_elapsed: Label = %TimeElapsed
 @onready var level_name: Label = %LevelName
-@onready var best_coins_collected: Label = %BestCoinsCollected
-@onready var best_time_elapsed: Label = %BestTimeElapsed
+@onready var coins: Label = %Coins
+@onready var time: Label = %Time
+@onready var best_coins: Label = %BestCoins
+@onready var new_best_coins: Label = %NewBestCoins
+@onready var best_time: Label = %BestTime
+@onready var new_best_time: Label = %NewBestTime
 
 
 func _ready() -> void:
 	next_level.grab_focus()
 	level_name.text = GameManager.get_level_name()
 	var level_score: Dictionary = GameManager.get_level_score()
-	coins_collected.text = str(level_score["coins"])
-	best_coins_collected.text = str(level_score["best_coins"])
-	time_elapsed.text = str(level_score["time_elapsed"] / 1000)
-	best_time_elapsed.text = str(level_score["best_time_elapsed"] / 1000)
+	coins.text = str(level_score["coins"])
+	best_coins.text = str(level_score["best_coins"])
+	time.text = str(level_score["time_elapsed"] / 1000)
+	best_time.text = str(level_score["best_time_elapsed"] / 1000)
+	new_best_coins.visible = coins.text == best_coins.text
+	new_best_time.visible = time.text == best_time.text
 
 
 func _on_next_level_pressed() -> void:
