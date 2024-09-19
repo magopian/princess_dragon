6c43cff3a75d8dc81253e1bb3afa47368c627951
Mathieu Agopian <mathieu@agopian.info>
2024-09-19 16:03:12 +0200

    Add an entry to the options screen in the main screen

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -18,5 +18,9 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/savegame_selection.tscn")
 
 
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -5,3 +5,13 @@ func _ready() -> void:
 	var user_prefs: UserPreferences = GameManager.user_prefs
 	%Muted.button_pressed = user_prefs.muted
 	%Muted.toggled.connect(GameManager.muted_preference_changed.emit)
+	%MainMenu.grab_focus()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("menu"):
+		_on_main_menu_pressed()
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
