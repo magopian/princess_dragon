fcbf153fd9e71ae8653281dc7b8aeff02798b737
Mathieu Agopian <mathieu@agopian.info>
2024-09-27 10:14:32 +0200

    Add juice to the countdown timer: scale down and transparent

--- scripts/count_down_timer.gd
+++ scripts/count_down_timer.gd
@@ -9,10 +9,13 @@ func _ready() -> void:
 	%StartNow.pressed.connect(start_level)
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	%LevelName.text = GameManager.get_level_name()
 	var time_left: int = ceil(%Timer.time_left)
-	%StartingIn.text = str(time_left)
+
+	if str(time_left) != %StartingIn.text:
+		animate_new_number()
+		%StartingIn.text = str(time_left)
 
 
 func _on_timeout() -> void:
@@ -23,3 +26,14 @@ func start_level() -> void:
 	visible = false
 	get_tree().paused = false
 	GameManager.level_started.emit()
+
+
+func animate_new_number() -> void:
+	%StartingIn.scale = Vector2(2.5, 2.5)
+	%StartingIn.modulate.a = 1
+	var tween: Tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(%StartingIn, "scale", Vector2(1, 1), 0.9)
+	tween.tween_property(%StartingIn, "modulate:a", 0, 0.9)
